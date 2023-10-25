d2f8d096ff677be9e1058d2696cf30130fa9c094
Mark Cassidy <mark@cassidy.dk>
2023-10-25 16:54:26 +0200

    zinvite handler 0.1 alpha

--- sitecorebot/app.py
+++ sitecorebot/app.py
@@ -8,7 +8,37 @@ SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
 app = App(token=SLACK_BOT_TOKEN)
 
-bot_admins = ["U0D8XHJSH", "lala"]
+bot_admins = ["U0D8XHJSH", "U09SJ4FGW"]
+channel_zinvite = ["CA6SNF4NQ", "C0625KEQ2VD"]
+
+email_regex_pattern = "[\w\.-]+@[\w\.-]+\.\w+"
+
+@app.message("New User Request")
+def app_new_user_request(message, say):
+    channel_id = message["channel"]
+    message_text = message["text"]
+    message_ts = message["ts"]
+
+    if channel_id not in channel_zinvite:
+        print(f"New User Request ignored in channel {channel_id}")
+        return
+
+    match = re.search(email_regex_pattern, message_text)
+    if match:
+        user_request_email = match.group()
+        print(f"Incoming New User Request for Email {user_request_email}")
+        try:
+            userinfo_request = app.client.users_lookupByEmail(email=user_request_email)
+            existing_userinfo = userinfo_request["user"]
+            app.client.reactions_add(channel=channel_id, timestamp=message["ts"], name="red_circle")
+            response_message = f"Found user as <@{existing_userinfo['id']}> ({existing_userinfo['real_name']})"
+            say(text=response_message, thread_ts=message_ts)
+            say(text = f"```{existing_userinfo}```", thread_ts=message_ts)
+            print(response_message)
+        except:
+            # user was not found, this is good
+            app.client.reactions_add(channel=channel_id, timestamp=message["ts"], name="large_green_circle")
+            print("This is a new user")
 
 # reference message pattern implementation
 @app.message(re.compile("([hH]ey|[hH]ello) [sS]itecore[bB]ot"))
@@ -34,6 +64,16 @@ def all_message_handler(message, say):
     # see channelinfo.json
     channel = app.client.conversations_info(channel=channel_id, include_num_members=True)["channel"]
     print(f"Message id {message_id} received in channel #{channel['name']} ({channel_id}) ({channel['num_members']} members) at {dt}. '{message_text}' from user {userinfo['name']} ({user_id}).")
+
+# catch-all
+@app.event("message")
+def handle_message_events(body, logger):
+    print(body)
+
+# not doing anything with @mentions of the bot yet
+@app.event("app_mention")
+def handle_app_mention_events(body, logger):
+    print(body)
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, SLACK_APP_TOKEN)
