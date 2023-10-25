c4bb4a094760b64e39e8cfa0ac343001bd6fb8c5
Mark Cassidy <mark@cassidy.dk>
2023-10-25 23:44:29 +0200

    Added longhorntaco to the bot admin list

--- sitecorebot/defs.py
+++ sitecorebot/defs.py
@@ -6,9 +6,10 @@ user_akshaysura = "U09SJ4FGW"
 user_sitecorejunkie = "U09SJGX5X"
 user_jammykam = "U0A1FKM24"
 user_michaelwest = "U09SK9G1F"
+user_longhorntaco = "U0C29A1MG"
 
 email_regex_pattern = "[\w\.-]+@[\w\.-]+\.\w+"
 
 # No special priveleges implemented for admins yet
-bot_admins = [user_cassidydotdk, user_akshaysura, user_sitecorejunkie, user_jammykam, user_michaelwest]
+bot_admins = [user_cassidydotdk, user_akshaysura, user_sitecorejunkie, user_jammykam, user_michaelwest, user_longhorntaco]
 im_channels = ["im", "mpim"]
