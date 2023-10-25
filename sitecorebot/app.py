e1260b1f4d4e3d717ce71fc275dd3b1af67d4a41
Mark Cassidy <mark@cassidy.dk>
2023-10-25 14:11:13 +0200

    added universal listener, to figure out data formats and structures

--- sitecorebot/app.py
+++ sitecorebot/app.py
@@ -1,6 +1,8 @@
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import os
+import re
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,15 +11,21 @@ SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
 
 app = App(token=SLACK_BOT_TOKEN)
 
-@app.event("app_mention")
-def app_mention_handler(body, say):
-    say("Poland has mountains!")
-
-@app.message("mountains")
-def app_message_mountains(message, say):
-    say(text=f"You know <@{message['user']}>, Poland has mountains too!")
+@app.message(re.compile("^."))
+def all_message_handler(message, say):
+    # see message.json
+    message_id = message["client_msg_id"]
+    message_text = message["text"]
+    user_id = message["user"]
+    # see userinfo.json
+    userinfo = app.client.users_info(user=user_id)["user"]
+    timestamp = message["ts"]
+    dt = time.ctime(float(timestamp))
+    channel_id = message["channel"]
+    # see channelinfo.json
+    channel = app.client.conversations_info(channel=channel_id, include_num_members=True)["channel"]
+    print(f"Message id {message_id} received in channel #{channel['name']} at {dt}. '{message_text}' from user {userinfo['name']}.")
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, SLACK_APP_TOKEN)
     handler.start()
-
