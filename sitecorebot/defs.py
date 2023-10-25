7c8081cd665e1007c4ae2a8eaa18108610c1cf86
Mark Cassidy <mark@cassidy.dk>
2023-10-25 18:20:17 +0200

    added michael west to bot admins

--- sitecorebot/defs.py
+++ sitecorebot/defs.py
@@ -5,8 +5,9 @@ user_cassidydotdk = "U0D8XHJSH"
 user_akshaysura = "U09SJ4FGW"
 user_sitecorejunkie = "U09SJGX5X"
 user_jammykam = "U0A1FKM24"
+user_michaelwest = "U09SK9G1F"
 
 email_regex_pattern = "[\w\.-]+@[\w\.-]+\.\w+"
 
 # No special priveleges implemented for admins yet
-bot_admins = [user_cassidydotdk, user_akshaysura, user_sitecorejunkie, user_jammykam]
+bot_admins = [user_cassidydotdk, user_akshaysura, user_sitecorejunkie, user_jammykam, user_michaelwest]
