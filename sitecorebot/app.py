1299542defa07386d7e6f0b000f790f6d45eecaf
Mark Cassidy <mark@cassidy.dk>
2023-10-25 17:19:41 +0200

    lil' bit of code cleanup

--- sitecorebot/app.py
+++ sitecorebot/app.py
@@ -8,8 +8,16 @@ SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
 app = App(token=SLACK_BOT_TOKEN)
 
-bot_admins = ["U0D8XHJSH", "U09SJ4FGW"]
-channel_zinvite = ["CA6SNF4NQ", "C0625KEQ2VD"]
+channel_sitecorebot_sandbox = "C0625KEQ2VD"
+channel_zinvite = "CA6SNF4NQ"
+user_cassidydotdk = "U0D8XHJSH"
+user_akshaysura = "U09SJ4FGW"
+user_sitecorejunkie = "U09SJGX5X"
+
+# No special priveleges implemented for admins yet
+bot_admins = [user_cassidydotdk, user_akshaysura]
+# Will be listening to "New User Request in #zinvite and #sitecorebot-sandbox"
+channel_zinvite = [channel_zinvite, channel_sitecorebot_sandbox]
 
 email_regex_pattern = "[\w\.-]+@[\w\.-]+\.\w+"
 
@@ -20,7 +28,10 @@ def app_new_user_request(message, say):
     message_ts = message["ts"]
 
     if channel_id not in channel_zinvite:
-        print(f"New User Request ignored in channel {channel_id}")
+        channel = app.client.conversations_info(channel=channel_id, include_num_members=True)["channel"]
+        ignored_message = f"New User Request ignored in channel {channel['name']} ({channel_id})"
+        app.logger.debug(ignored_message)
+        print(ignored_message)
         return
 
     match = re.search(email_regex_pattern, message_text)
@@ -30,23 +41,26 @@ def app_new_user_request(message, say):
         try:
             userinfo_request = app.client.users_lookupByEmail(email=user_request_email)
             existing_userinfo = userinfo_request["user"]
-            app.client.reactions_add(channel=channel_id, timestamp=message["ts"], name="red_circle")
+            app.client.reactions_add(channel=channel_id, timestamp=message_ts, name="red_circle")
             response_message = f"Found user as <@{existing_userinfo['id']}> ({existing_userinfo['real_name']})"
             say(text=response_message, thread_ts=message_ts)
             say(text = f"```{existing_userinfo}```", thread_ts=message_ts)
             print(response_message)
         except:
             # user was not found, this is good
-            app.client.reactions_add(channel=channel_id, timestamp=message["ts"], name="large_green_circle")
-            print("This is a new user")
+            app.client.reactions_add(channel=channel_id, timestamp=message_ts, name="large_green_circle")
+            print(f"Incoming request for {user_request_email} is a new user")
 
 # reference message pattern implementation
 @app.message(re.compile("([hH]ey|[hH]ello) [sS]itecore[bB]ot"))
 def app_sitecorebot_referred(message, say):
+    channel_id = message["channel"]
+    message_ts = message["ts"]
+
     if message["user"] not in bot_admins:
-        print("Sitecorebot was called by a non-admin.")
+        app.client.reactions_add(channel=channel_id, timestamp=message_ts, name="thumbsdown")
     else:
-        print("Sitecorebot was called by an admin")
+        app.client.reactions_add(channel=channel_id, timestamp=message_ts, name="thumbsup")
 
 # Universal listener, for debug and development purposes
 # Only gets invoked if no previous handler has picked up the message
@@ -69,11 +83,13 @@ def all_message_handler(message, say):
 @app.event("message")
 def handle_message_events(body, logger):
     print(body)
+    print()
 
 # not doing anything with @mentions of the bot yet
 @app.event("app_mention")
 def handle_app_mention_events(body, logger):
     print(body)
+    print()
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, SLACK_APP_TOKEN)
