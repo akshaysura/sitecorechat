899d02d282bd2af00090015f2dcd56b9304d65f8
Mark Cassidy <mark@cassidy.dk>
2023-10-27 21:01:28 +0200

    added marekbot and tamas tarnok to bot admin list

--- sitecorebot/user.py
+++ sitecorebot/user.py
@@ -5,14 +5,14 @@ user_sitecorejunkie = "U09SJGX5X"
 user_jammykam = "U0A1FKM24"
 user_michaelwest = "U09SK9G1F"
 user_longhorntaco = "U0C29A1MG"
-user_marekmusielak = ""
-user_tamastarnok = ""
+user_marekmusielak = "U09UVE83Y"
+user_tamastarnok = "U3H1FSTU2"
 
 # mvp program coordinators
 user_tamasvarga = "U0BAH3W05"
 user_nicolemontero = ""
 
-bot_admins = [user_cassidydotdk, user_akshaysura, user_sitecorejunkie, user_jammykam, user_michaelwest, user_longhorntaco]
+bot_admins = [user_cassidydotdk, user_akshaysura, user_sitecorejunkie, user_jammykam, user_michaelwest, user_longhorntaco, user_marekmusielak, user_tamastarnok]
 mvp_coordinators = [user_tamasvarga, user_nicolemontero]
 
 class User:
