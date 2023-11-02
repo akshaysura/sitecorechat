62e2357b3bb467b09c43a106192ccba6a6a7314e
Mark Cassidy <mark@cassidy.dk>
2023-11-02 15:33:16 +0100

    fix mishandled bot_message

--- sitecorebot/message.py
+++ sitecorebot/message.py
@@ -16,7 +16,8 @@ class Message:
     
     @property
     def is_bot_message(self) -> bool:
-        return self._message["subtype"] == "bot_message"
+        return False
+#        return self._message["subtype"] == "bot_message"
 
     @property
     def is_im(self) -> bool:
